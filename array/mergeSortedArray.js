97906a71ea9ade1bcc8e9c54856976618e9a5915
boghdady <boghdady107@gmail.com>
2022-06-09 14:27:24 +0200

    hash-tables

--- array/mergeSortedArray.js
+++
@@ -1,9 +0,0 @@
-// Problem: Merge sorted array
-// Input: [0, 3, 4, 31] , [4, 6, 30]
-// Output: [0, 3, 4, 4, 6, 30, 31]
-
-function mergeSortedArray(arr1, arr2) {
-  return [...arr1, ...arr2].sort((a, b) => a - b);
-}
-
-console.log(mergeSortedArray([0, 3, 4, 31], [4, 6, 30]));
